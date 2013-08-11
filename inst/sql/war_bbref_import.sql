83c6c2e03c8a47c81337b3866631b158fe362992
beanumber <ben.baumer@gmail.com>
2013-08-11 09:43:00 -0400

    removed SQL code

--- inst/sql/war_bbref_import.sql
+++
@@ -1,3 +0,0 @@
-
-LOAD DATA LOCAL INFILE "../data/war_bbref_bat.txt" INTO TABLE war_bbref_batting FIELDS TERMINATED BY "," IGNORE 1 lines;
-LOAD DATA LOCAL INFILE "../data/war_bbref_pitch.txt" INTO TABLE war_bbref_pitching FIELDS TERMINATED BY "," IGNORE 1 lines;
