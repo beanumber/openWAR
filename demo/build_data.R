cfeef6b79a9747effc52ba43b017cc9bc1851338
Ben Baumer <ben.baumer@gmail.com>
2013-08-14 08:30:34 -0400

    updated build_data.R script

--- demo/build_data.R
+++ demo/build_data.R
@@ -18,5 +18,7 @@ openWAR.2013 = getWAR(MLBAM2013)
 save(openWAR.2013, file="data/openWAR.2013.rda")
 
 # Generated the uncertainty estimates
-openWAR.2013.sim = shakeWAR(ds, N = 1000)
+ds = makeWAR(MLBAM2013)
+# Will take a loooooong time
+openWAR.2013.sim = shakeWAR(ds, N = 2000)
 save(openWAR.2013.sim, file="data/openWAR.2013.sim.rda")
