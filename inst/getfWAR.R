37f90ad6196414170878fd1ef69dcc3d166daedc
beanumber <ben.baumer@gmail.com>
2013-09-15 20:48:56 -0400

    moved fWAR to data

--- inst/getfWAR.R
+++ inst/getfWAR.R
@@ -1,23 +1,4 @@
-#' @title getfWAR
-#' 
-#' @description Loads fWAR from FanGraphs.com
-#' 
-#' @details Retrieves fWAR figures from a prior download
-#' 
-#' @return a data.frame consisting of fWAR
-#' 
-#' @export
-#' @examples
-#' 
-#' # Get data from yesterday
-#' fWAR = getfWAR()
-#' require(mosaic)
-#' densityplot(~WAR, data=fWAR)
-#' 
-#' # Relationship between batting and fielding
-#' xyplot(RAA_field ~ RAA_bat, data=fWAR, type=c("p", "r", "smooth"))
 
-getfWAR <- function () {
   bat = read.csv("data/FanGraphs_2012_batting.csv")
   pitch = read.csv("data/FanGraphs_2012_pitching.csv")
   out = merge(x=bat, y=pitch, by = "playerid", all=TRUE)
@@ -27,7 +8,7 @@ getfWAR <- function () {
   out$RAA_field = with(out, ifelse(is.na(Fielding), 0, Fielding))
   out$WAR_pitch = with(out, ifelse(is.na(WAR.y), 0, WAR.y))
   out$WAR = with(out, ifelse(is.na(WAR.x), 0, WAR.x) + WAR_pitch)
-  out = out[, c("playerid", "Name", "WAR", "RAA_bat", "RAA_br", "RAA_field", "WAR_pitch", "WAR")]
-  return(out)
-}
-
+  fWAR = out[, c("playerid", "Name", "WAR", "RAA_bat", "RAA_br", "RAA_field", "WAR_pitch", "WAR")]
+  
+  
+save(fWAR, file="data/fWAR.rda")
