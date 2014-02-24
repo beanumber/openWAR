63bcae123b5399850c74e97776878ed6c43e5926
gjm112 <gjm112@gmail.com>
2014-02-24 16:00:18 -0500

    Added my middle initial.

--- R/openWAR-package.R
+++ R/openWAR-package.R
@@ -4,7 +4,7 @@
 #' @description This package allows a user to explore WAR
 #' 
 #' @details A fully open-source implementation of WAR. 
-#' @author Ben Baumer and Gregory Matthews
+#' @author Ben Baumer and Gregory J. Matthews
 #' 
 #' @docType package
 #' @import mosaic plyr stringr XML RCurl Sxslt
