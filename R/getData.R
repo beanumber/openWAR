05482bb5da9e302b64e3286c1ecfd7ce4f304b69
beanumber <ben.baumer@gmail.com>
2014-12-19 10:14:15 -0500

    plyr -> dplyr in getData.R

--- R/getData.R
+++ R/getData.R
@@ -40,12 +40,13 @@ getData <- function(start = Sys.Date()-1, end = NULL, gameIds = NULL, drop.suspe
   #}
   ds.list = lapply(gd.list, "[[", "ds")
   out = do.call(rbind, ds.list)
-  out = subset(out, game_type == "R")
+  out = filter(out, game_type == "R")
   # exclude suspended games
   if (drop.suspended) {
-    test = ddply(out, ~gameId, summarise, Innings = max(inning))
-    suspended = subset(test, Innings < 5)$gameId
-    out = subset(out, !gameId %in% suspended)
+#    test = ddply(out, ~gameId, summarise, Innings = max(inning))
+    test <- summarise(group_by(out, gameId), Innings = max(inning))
+    suspended = filter(test, Innings < 5)$gameId
+    out = filter(out, !gameId %in% suspended)
   }
   
   # Set the class attribute
