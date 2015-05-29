6cd2c8739a0c6d42ec4ca1712768c51a97cf8cd8
Ben Baumer <ben.baumer@gmail.com>
2015-05-29 10:48:32 -0400

    made references do dplyr::do explicit

--- R/makeWAR.R
+++ R/makeWAR.R
@@ -341,9 +341,9 @@ makeWARBaserunning = function(data, mod.bat, verbose = TRUE, ...) {
     # ds3Probs = plyr::ddply(ds3, ~event + startCode + startOuts, getCDF) ds2Probs = ddply(ds2, ~event + startCode + startOuts,
     # getCDF) ds1Probs = ddply(ds1, ~event + startCode + startOuts, getCDF)
     
-    ds3Probs <- do(group_by(ds3, event, startCode, startOuts), getCDF(.))
-    ds2Probs <- do(group_by(ds2, event, startCode, startOuts), getCDF(.))
-    ds1Probs <- do(group_by(ds1, event, startCode, startOuts), getCDF(.))
+    ds3Probs <- dplyr::do(group_by(ds3, event, startCode, startOuts), getCDF(.))
+    ds2Probs <- dplyr::do(group_by(ds2, event, startCode, startOuts), getCDF(.))
+    ds1Probs <- dplyr::do(group_by(ds1, event, startCode, startOuts), getCDF(.))
     
     # Merge onto the main data frame
     join.idx = c("event", "startCode", "startOuts")
