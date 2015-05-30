f13b05d94762b1be5105c0c3b60aa29fbec54963
Ben Baumer <ben.baumer@gmail.com>
2015-05-29 21:42:02 -0400

    added new test

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -4,8 +4,11 @@ library(openWAR)
 #  expect_is(gameday(), "gameday")
 #})
 
-
 test_that("you can plot GameDayPlays", {
   expect_is(May14, "GameDayPlays")
   expect_is(plot(May14), "trellis")
+})
+
+test_that("getRunEx is a function", {
+  expect_is(getRunEx(May14), "function")
 })
