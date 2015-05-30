0aaee25b557de79f5a5430a7c2d0072b4f358a16
Ben Baumer <ben.baumer@gmail.com>
2015-05-29 22:23:27 -0400

    moved ancillary data to openWARData package

--- data-raw/MLBAM.R
+++
@@ -1,9 +0,0 @@
-library(openWAR)
-
-# Get the data for 2012
-MLBAM2012 = getData(start = "2013-03-28", end = "2013-10-03")
-save(MLBAM2012, file = "data/MLBAM2012.rda")
-
-# Get the data for 2013
-MLBAM2013 = getData(start = "2013-03-31", end = "2013-09-30")
-save(MLBAM2013, file = "data/MLBAM2013.rda")
--- data-raw/idBP.R
+++
@@ -1,9 +0,0 @@
-
-
-bp.ids <- read.csv("http://www.baseballprospectus.com/sortable/playerids/playerid_list.csv")
-names(bp.ids) <- c("nameLast", "nameFirst", "bpId", "davenportId", "mlbamId", "retroId")
-ids = merge(x = Lahman::Master, y = bp.ids, by.x = "retroID", by.y = "retroId", all = TRUE)
-
-idBP <- filter(ids, !is.na(mlbamId))
- 
-save(idBP, file = "data/idBP.rda")
