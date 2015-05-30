10be8a8bfe2fef0f3981b6df263392515446e48c
Ben Baumer <ben.baumer@gmail.com>
2015-05-29 23:07:49 -0400

    moved two vignettes to openWARData

--- data-raw/simulations.R
+++ data-raw/simulations.R
@@ -1,8 +1,7 @@
 library(openWAR)
 
-data(MLBAM2013)
-# Generated the uncertainty estimates
-madeWAR = makeWAR(MLBAM2013)
+data(May)
+
 # Will take a loooooong time
-openWAR.2013.sim = shakeWAR(madeWAR, N = 10)
-save(openWAR.2013.sim, file="data/openWAR.2013.sim.rda")
+openWAR.2013.sim = shakeWAR(May, N = 10)
+summary(openWAR.2013.sim)
