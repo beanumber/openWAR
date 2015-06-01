cf5f77d5168e8ec2f1b5d1cd5e8b38622b631b68
Ben Baumer <ben.baumer@gmail.com>
2015-06-01 14:23:02 -0400

    moved crosscheck() to openWARData

--- R/GameDayPlays.R
+++ R/GameDayPlays.R
@@ -151,6 +151,7 @@ summary.GameDayPlays = function(data) {
 #' @return A data.frame of seasonal totals for each team
 #' 
 #' @export tabulate.GameDayPlays
+#' @export tabulate
 #' @examples
 #' 
 #' ds = getData()
@@ -168,65 +169,15 @@ tabulate.GameDayPlays = function(data) {
     # !event %in% c('Sac Bunt', 'Sacrifice Bunt DP')) , SLG = (sum(event == 'Single') + 2*sum(event == 'Double') + 3*sum(event
     # == 'Triple') + 4*sum(event == 'Home Run') ) / sum(isAB) )
     
-    data %>% mutate(bat_team = ifelse(half == "top", as.character(away_team), as.character(home_team))) %>% mutate(yearId = as.numeric(substr(gameId, 
-        start = 5, stop = 8))) %>% group_by(yearId, bat_team) %>% summarise(G = length(unique(gameId)), PA = sum(isPA), AB = sum(isAB), 
-        R = sum(runsOnPlay), H = sum(isHit), HR = sum(event == "Home Run"), BB = sum(event %in% c("Walk", "Intent Walk")), K = sum(event %in% 
-            c("Strikeout", "Strikeout - DP")), BA = sum(isHit)/sum(isAB), OBP = sum(isHit | event %in% c("Walk", "Intent Walk", 
-            "Hit By Pitch"))/sum(isPA & !event %in% c("Sac Bunt", "Sacrifice Bunt DP")), SLG = (sum(event == "Single") + 2 * 
-            sum(event == "Double") + 3 * sum(event == "Triple") + 4 * sum(event == "Home Run"))/sum(isAB))
+    data %>% 
+      mutate(bat_team = factor(ifelse(half == "top", as.character(away_team), as.character(home_team)))) %>% 
+      mutate(yearId = as.numeric(substr(gameId, start = 5, stop = 8))) %>% 
+      group_by(yearId, bat_team) %>% 
+      summarise(G = length(unique(gameId)), PA = sum(isPA), AB = sum(isAB), 
+        R = sum(runsOnPlay), H = sum(isHit), HR = sum(event == "Home Run"), 
+        BB = sum(event %in% c("Walk", "Intent Walk")), K = sum(event %in% c("Strikeout", "Strikeout - DP")), 
+        BA = sum(isHit)/sum(isAB), 
+        OBP = sum(isHit | event %in% c("Walk", "Intent Walk", "Hit By Pitch"))/sum(isPA & !event %in% c("Sac Bunt", "Sacrifice Bunt DP")), 
+        SLG = (sum(event == "Single") + 2 * sum(event == "Double") + 3 * sum(event == "Triple") + 4 * sum(event == "Home Run"))/sum(isAB)
+        )
 }
-
-
-#' @title crosscheck.GameDayPlays
-#' 
-#' @description Cross-check the accuracy of the GameDay data with the Lahman database
-#' 
-#' @details Cross-checks summary statistics with the Lahman database. 
-#' 
-#' @param data An MLBAM data set
-#' 
-#' @return The ratio of the Frobenius norm of the matrix of differences to the Frobenius norm of the matrix
-#' defined by the Lahman database. 
-#' 
-#' @export crosscheck.GameDayPlays
-#' 
-#' 
-#' @examples
-#' 
-#' ds = getData()
-#' crosscheck(ds)
-#' 
-#' 
-crosscheck = function(data) UseMethod("crosscheck")
-
-crosscheck.GameDayPlays = function(data) {
-    
-    teams = tabulate(data)
-    
-    
-    lteams <- Lahman::Batting %>% 
-      group_by(yearID, teamID) %>% 
-      summarise(PA = sum(AB + BB + HBP + SH + SF, na.rm = TRUE), AB = sum(AB, 
-        na.rm = TRUE), R = sum(R, na.rm = TRUE), H = sum(H, na.rm = TRUE), HR = sum(HR, na.rm = TRUE), BB = sum(BB, na.rm = TRUE), 
-        K = sum(SO, na.rm = TRUE), BA = sum(H, na.rm = TRUE)/sum(AB, na.rm = TRUE), OBP = sum(H + BB + HBP, na.rm = TRUE)/sum(AB + 
-            BB + HBP + SF, na.rm = TRUE), SLG = sum(H + X2B + X3B + HR, na.rm = TRUE)/sum(AB, na.rm = TRUE))
-    
-    lteams = merge(x = lteams, y = Lahman::Teams[, c("yearID", "teamID", "G")], by = c("yearID", "teamID"))
-    lteams <- mutate(lteams, teamId = tolower(teamID))
-    lteams <- mutate(lteams, teamId = ifelse(teamId == "laa", "ana", as.character(teamId)))
-    
-    match = merge(x = teams, y = lteams, by.x = c("yearId", "bat_team"), by.y = c("yearID", "teamId"), all.x = TRUE)
-    
-    # move this out of here eventually 
-    # require(xtable) x = xtable(match[,c('bat_team', 'G.x', 'PA.x', 'AB.x', 'R.x', 'H.x',
-    # 'HR.x', 'BB.x', 'K.x', 'G.y', 'PA.y', 'AB.y', 'R.y', 'H.y', 'HR.y', 'BB.y', 'K.y')] , caption=c('Cross-check between MLBAM
-    # data (left) and Lahman data (right), 2012'), label='tab:crosscheck' , align = rep('c', 18)) print(x,
-    # include.rownames=FALSE)
-    
-    A = as.matrix(match[, c("G.x", "PA.x", "AB.x", "R.x", "H.x", "HR.x", "BB.x", "K.x")])
-    B = as.matrix(match[, c("G.y", "PA.y", "AB.y", "R.y", "H.y", "HR.y", "BB.y", "K.y")])
-    return(norm(A - B, "F")/norm(B, "F"))
-}
-
-
-
