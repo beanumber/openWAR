f35fc1ddbcdd2e8c4e464e43c02a90e2b8013cc0
davidbmitchell <david.b.mitchell@icloud.com>
2015-06-09 19:16:33 -0400

    fixed R CMD CHECK "NOTE" messages in openWARPlays.R

--- R/openWARPlays.R
+++ R/openWARPlays.R
@@ -33,9 +33,9 @@ getRAA.openWARPlays = function(data, ...) {
         
     # dplyr syntax
     war.bat <- data %>% 
-      group_by(batterId) %>% 
-      summarise(playerId = batterId[1], Name = max(as.character(batterName)), PA.bat = sum(isPA), 
-        G = length(unique(gameId)), HR = sum(event == "Home Run"), RAA.bat = sum(raa.bat, na.rm = TRUE))
+      group_by_(~batterId) %>% 
+      summarise_(playerId = ~batterId[1], Name = ~max(as.character(batterName)), PA.bat = ~sum(isPA), 
+        G = ~length(unique(gameId)), HR = ~sum(event == "Home Run"), RAA.bat = ~sum(raa.bat, na.rm = TRUE))
     
     # Baserunning war.br0 = ddply(data, ~batterId, summarise, RAA.br0 = sum(raa.br0, na.rm=TRUE)) war.br1 = ddply(subset(data,
     # !is.na(start1B)), ~start1B, summarise, PA.br1 = sum(isPA), RAA.br1 = sum(raa.br1, na.rm=TRUE)) war.br2 =
@@ -43,14 +43,20 @@ getRAA.openWARPlays = function(data, ...) {
     # = ddply(subset(data, !is.na(start3B)), ~start3B, summarise, PA.br3 = sum(isPA), RAA.br3 = sum(raa.br3, na.rm=TRUE))
     
     # dplyr
-    war.br1 <- data %>% dplyr::filter(!is.na(start1B)) %>% group_by(start1B) %>% summarise(PA.br1 = sum(isPA), RAA.br1 = sum(raa.br1, 
-        na.rm = TRUE))
+    war.br1 <- data %>% 
+    	dplyr::filter_(~!is.na(start1B)) %>% 
+    	group_by_(~start1B) %>% 
+    	summarise_(PA.br1 = ~sum(isPA), RAA.br1 = ~sum(raa.br1, na.rm = TRUE))
     
-    war.br2 <- data %>% dplyr::filter(!is.na(start2B)) %>% group_by(start2B) %>% summarise(PA.br2 = sum(isPA), RAA.br2 = sum(raa.br2, 
-        na.rm = TRUE))
+    war.br2 <- data %>% 
+    	dplyr::filter_(~!is.na(start2B)) %>% 
+    	group_by_(~start2B) %>% 
+    	summarise_(PA.br2 = ~sum(isPA), RAA.br2 = ~sum(raa.br2, na.rm = TRUE))
     
-    war.br3 <- data %>% dplyr::filter(!is.na(start3B)) %>% group_by(start3B) %>% summarise(PA.br3 = sum(isPA), RAA.br3 = sum(raa.br3, 
-        na.rm = TRUE))
+    war.br3 <- data %>% 
+    	dplyr::filter_(~!is.na(start3B)) %>% 
+    	group_by_(~start3B) %>% 
+    	summarise_(PA.br3 = ~sum(isPA), RAA.br3 = ~sum(raa.br3, na.rm = TRUE))
     
     
     # Fielding war.P = ddply(data, ~pitcherId, summarise, PA.P = sum(isPA), RAA.P = sum(raa.P, na.rm=TRUE)) war.C = ddply(data,
@@ -62,15 +68,33 @@ getRAA.openWARPlays = function(data, ...) {
     # ~playerId.CF, summarise, PA.CF = sum(isPA), RAA.CF = sum(raa.CF, na.rm=TRUE)) war.RF = ddply(data, ~playerId.RF,
     # summarise, PA.RF = sum(isPA), RAA.RF = sum(raa.RF, na.rm=TRUE))
     
-    war.P <- data %>% group_by(pitcherId) %>% summarise(PA.P = sum(isPA), RAA.P = sum(raa.P, na.rm = TRUE))
-    war.C <- data %>% group_by(playerId.C) %>% summarise(PA.C = sum(isPA), RAA.C = sum(raa.C, na.rm = TRUE))
-    war.1B <- data %>% group_by(playerId.1B) %>% summarise(PA.1B = sum(isPA), RAA.1B = sum(raa.1B, na.rm = TRUE))
-    war.2B <- data %>% group_by(playerId.2B) %>% summarise(PA.2B = sum(isPA), RAA.2B = sum(raa.2B, na.rm = TRUE))
-    war.3B <- data %>% group_by(playerId.3B) %>% summarise(PA.3B = sum(isPA), RAA.3B = sum(raa.3B, na.rm = TRUE))
-    war.SS <- data %>% group_by(playerId.SS) %>% summarise(PA.SS = sum(isPA), RAA.SS = sum(raa.SS, na.rm = TRUE))
-    war.LF <- data %>% group_by(playerId.LF) %>% summarise(PA.LF = sum(isPA), RAA.LF = sum(raa.LF, na.rm = TRUE))
-    war.CF <- data %>% group_by(playerId.CF) %>% summarise(PA.CF = sum(isPA), RAA.CF = sum(raa.CF, na.rm = TRUE))
-    war.RF <- data %>% group_by(playerId.RF) %>% summarise(PA.RF = sum(isPA), RAA.RF = sum(raa.RF, na.rm = TRUE))
+    war.P <- data %>% 
+    	group_by_(~pitcherId) %>% 
+    	summarise_(PA.P = ~sum(isPA), RAA.P = ~sum(raa.P, na.rm = TRUE))
+    war.C <- data %>% 
+    	group_by_(~playerId.C) %>% 
+    	summarise_(PA.C = ~sum(isPA), RAA.C = ~sum(raa.C, na.rm = TRUE))
+    war.1B <- data %>% 
+    	group_by_(~playerId.1B) %>% 
+    	summarise_(PA.1B = ~sum(isPA), RAA.1B = ~sum(raa.1B, na.rm = TRUE))
+    war.2B <- data %>% 
+    	group_by_(~playerId.2B) %>% 
+    	summarise_(PA.2B = ~sum(isPA), RAA.2B = ~sum(raa.2B, na.rm = TRUE))
+    war.3B <- data %>% 
+    	group_by_(~playerId.3B) %>% 
+    	summarise_(PA.3B = ~sum(isPA), RAA.3B = ~sum(raa.3B, na.rm = TRUE))
+    war.SS <- data %>% 
+    	group_by_(~playerId.SS) %>% 
+    	summarise_(PA.SS = ~sum(isPA), RAA.SS = ~sum(raa.SS, na.rm = TRUE))
+    war.LF <- data %>% 
+    	group_by_(~playerId.LF) %>% 
+    	summarise_(PA.LF = ~sum(isPA), RAA.LF = ~sum(raa.LF, na.rm = TRUE))
+    war.CF <- data %>% 
+    	group_by_(~playerId.CF) %>% 
+    	summarise_(PA.CF = ~sum(isPA), RAA.CF = ~sum(raa.CF, na.rm = TRUE))
+    war.RF <- data %>% 
+    	group_by_(~playerId.RF) %>% 
+    	summarise_(PA.RF = ~sum(isPA), RAA.RF = ~sum(raa.RF, na.rm = TRUE))
     
     
     # Pitching
@@ -78,7 +102,9 @@ getRAA.openWARPlays = function(data, ...) {
     # war.pitch = ddply(data, ~ pitcherId, summarise, Name = max(as.character(pitcherName)), BF = sum(isPA), RAA.pitch =
     # sum(raa.pitch))
     
-    war.pitch <- data %>% group_by(pitcherId) %>% summarise(Name = max(as.character(pitcherName)), BF = sum(isPA), RAA.pitch = sum(raa.pitch))
+    war.pitch <- data %>% 
+    	group_by_(~pitcherId) %>% 
+    	summarise_(Name = ~max(as.character(pitcherName)), BF = ~sum(isPA), RAA.pitch = ~sum(raa.pitch))
     
     # Merge them all together
     
@@ -100,11 +126,11 @@ getRAA.openWARPlays = function(data, ...) {
     
     # clean up
     players[is.na(players)] = 0
-    players = mutate(players, RAA.br = RAA.br1 + RAA.br2 + RAA.br3)
-    players = mutate(players, RAA.off = RAA.bat + RAA.br)
-    players = mutate(players, RAA.field = RAA.P + RAA.C + RAA.1B + RAA.2B + RAA.3B + RAA.SS + RAA.LF + RAA.CF + RAA.RF)
-    players = mutate(players, RAA = RAA.bat + RAA.br + RAA.pitch + RAA.field)
-    players = mutate(players, TPA = PA.bat + BF)
+    players = mutate_(players, RAA.br = ~(RAA.br1 + RAA.br2 + RAA.br3))
+    players = mutate_(players, RAA.off = ~(RAA.bat + RAA.br))
+    players = mutate_(players, RAA.field = ~(RAA.P + RAA.C + RAA.1B + RAA.2B + RAA.3B + RAA.SS + RAA.LF + RAA.CF + RAA.RF))
+    players = mutate_(players, RAA = ~(RAA.bat + RAA.br + RAA.pitch + RAA.field))
+    players = mutate_(players, TPA = ~(PA.bat + BF))
     players = players[, setdiff(names(players), c("Name.x", "Name.y"))]
     class(players) = c("openWARPlayers", "data.frame")
     return(players)
