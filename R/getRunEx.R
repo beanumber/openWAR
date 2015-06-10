6d2f0a38eb5ce16cf28055ebf8368bb4d18381d6
Gregory Matthews <gjm112@gmail.com>
2015-06-10 13:09:59 -0500

    Added a note to the getRunEx.R documenation file about how to correctly specify a basecode.

--- R/getRunEx.R
+++ R/getRunEx.R
@@ -10,6 +10,11 @@
 #' 
 #' @return A function that takes two arguments: baseCode and outs, and returns a run value 
 #' 
+#' @note baseCode takes on integer values between 0 and 7 with each value correpsonding to a unique 
+#' base state.  The baseCode is the binary representation of the base state with first, second, and third base represented
+#' by 1, 2, and 4, respectively.  For example, runners on second and third corresponds to a baseCode of 6 (i.e. 2 + 4 = 6).  
+#'
+#'
 #' 
 #' @export
 #' @import dplyr
