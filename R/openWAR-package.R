e1b8139a7cf670a8f3f83aed2463aa9a860c8db3
Ben Baumer <ben.baumer@gmail.com>
2015-06-23 11:45:32 -0400

    close #69

--- R/openWAR-package.R
+++ R/openWAR-package.R
@@ -1,9 +1,15 @@
 #' @title openWAR
 #' @aliases openWAR openWAR-package
 #' 
-#' @description This package allows a user to explore WAR
+#' @description This package allows a user to explore \code{\link{openWAR}}
 #' 
-#' @details A fully open-source implementation of WAR. 
+#' @details A fully open-source implementation of WAR. Please see our paper on
+#' the arXiv or in the Journal of Quantitative Analysis in Sports for a complete
+#' description of \code{link{openWAR}}.
+#' 
+#' To learn more about openWAR, start with the vignettes: 
+#' \code{browseVignettes(package = "openWAR")}
+#' 
 #' @author Ben Baumer and Gregory J. Matthews
 #' 
 #' @docType package
