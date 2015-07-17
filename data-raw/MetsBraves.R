f06b74f30c4b26d963ac52b7f3329a058ed3f3ee
Ben Baumer <ben.baumer@gmail.com>
2015-07-17 15:40:59 -0400

    updated data files without "grouped_df" class attribute

--- data-raw/MetsBraves.R
+++ data-raw/MetsBraves.R
@@ -2,4 +2,4 @@ library(openWAR)
 
 # Get the data from a single game
 MetsBraves = gameday("gid_2012_08_12_atlmlb_nynmlb_1")
-save(MetsBraves, file = "data/MetsBraves.rdata", compress = "xz")
+save(MetsBraves, file = "data/MetsBraves.rda", compress = "xz")
