c846c9e74e6eb249ed87d291ccd6aa07e57a6d66
Gregory Matthews <gjm112@gmail.com>
2015-07-31 12:07:06 -0500

    Added a line about nteams in getWAR

--- R/getWAR.R
+++ R/getWAR.R
@@ -22,6 +22,8 @@
 #' }
 #' #openWAR and replacement level calculated with the same data set
 #' war = getWAR(May)
+#' #openWAR and replacement level calculated with the same data set
+#' #Replacement level calculated as if there were only 27.5 major league teams
 #' war = getWAR(May, nteams = 27.5)
 
 getWAR = function(data, dataRepl = NULL, nteams = 30, verbose = TRUE, ...) UseMethod("getWAR")
